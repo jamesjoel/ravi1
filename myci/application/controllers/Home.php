ab475b40a8124bedb3975758badcb04bb89f8c24
jamesjoel <james.joelj@gmail.com>
2019-04-29 16:51:46 +0530

    ci day 1

---
+++ myci/application/controllers/Home.php
@@ -0,0 +1,19 @@
+<?php
+class Home extends CI_Controller{
+
+	function index()
+	{
+		$this->load->view("home");
+	}
+	function about()
+	{
+		$this->load->view("about");
+	}
+	function contact()
+	{
+		$this->load->view("contact");
+	}
+
+
+}
+?>
---
+++ myci/application/views/about.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		body{
+			background-color: #BAC012;
+		}
+	</style>
+</head>
+<body>
+<h1>This is About Page</h1>
+</body>
+</html>
